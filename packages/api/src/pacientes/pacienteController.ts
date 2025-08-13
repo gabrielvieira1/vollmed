52a440d6a92ed28001b81660fc7e5fd06c44485e
Gabriel Vieira <gabrielvieira1@protonmail.com>
2025-08-13 00:31:21 -0300

    Refactor database queries in pacienteController to use parameterized statements for improved security and prevent SQL injection

--- packages/api/src/pacientes/pacienteController.ts
+++ packages/api/src/pacientes/pacienteController.ts
@@ -14,9 +14,11 @@ export const consultaPorPaciente = async (
   res: Response
 ): Promise<void> => {
   const { userInput } = req.query;
-  const query = `SELECT * FROM paciente WHERE nome = '${userInput}'`;
+  const query = `SELECT * FROM paciente WHERE nome = ?`;
   try {
-    const listaPacientes = await AppDataSource.manager.query(query);
+    const listaPacientes = await AppDataSource.manager.query(query, [
+      userInput,
+    ]);
     if (listaPacientes.length === 0) {
       res.status(404).json("Paciente não encontrado!");
     } else {
